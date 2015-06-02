3b7c35c262b2fc5c49cd0224333d395a7e923049
slavafleer <slavafleer@gmail.com>
2015-06-02 12:37:54 +0300

    added some comments

--- app/src/main/java/com/example/android/donotbelateapp/ParseApplication.java
+++ app/src/main/java/com/example/android/donotbelateapp/ParseApplication.java
@@ -7,6 +7,14 @@ import com.parse.Parse;
 /**
  * Created by Slava on 02/06/2015.
  */
+
+    // Learning:
+    // Till I didn't create this class, Parse.enableLocalDatastore(this) was
+    // crushing the app while changing device orientation with IllegalStateException.
+    // Also while creating this type class, need to add
+    // android:name="com.example.android.donotbelateapp.ParseApplication"
+    // in manifest.
+    
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
