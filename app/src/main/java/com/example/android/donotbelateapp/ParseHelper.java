042a53b4997c9263d8621c0134d5f83b82978d13
slavafleer <slavafleer@gmail.com>
2015-07-02 15:58:17 +0300

    o

---
+++ app/src/main/java/com/example/android/donotbelateapp/ParseHelper.java
@@ -0,0 +1,15 @@
+package com.example.android.donotbelateapp;
+
+/**
+ * Created by Slava on 02/07/2015.
+ */
+public class ParseHelper {
+
+    // ParseObject (Meeting) to Parcelable class
+
+
+
+
+    // Parcelable class to ParseObject (Meeting)
+
+}
