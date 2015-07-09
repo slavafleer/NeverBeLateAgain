e5c7d4edda6f03029a51c2da321ff8525dcfb819
slavafleer <slavafleer@gmail.com>
2015-07-09 12:58:52 +0300

    friends fragment now updating from global, need to delete not relevant code

--- app/src/main/java/com/example/android/donotbelateapp/ui/fragments/FriendsFragment.java
+++ app/src/main/java/com/example/android/donotbelateapp/ui/fragments/FriendsFragment.java
@@ -8,8 +8,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
 
+import com.example.android.donotbelateapp.GlobalApplication;
 import com.example.android.donotbelateapp.R;
-import com.example.android.donotbelateapp.ui.MainActivity;
+import com.example.android.donotbelateapp.model.parseCom.ParseConstants;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -19,7 +23,7 @@ import butterknife.InjectView;
  */
 public class FriendsFragment extends ListFragment {
 
-
+    private GlobalApplication Global;
 
     @InjectView(R.id.fragmentFriendsSpinner) ProgressBar mSpinner;
 
@@ -28,7 +32,11 @@ public class FriendsFragment extends ListFragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
         ButterKnife.inject(this, rootView);
+
+        Global = (GlobalApplication) getActivity().getApplication();
+
         mSpinner.setVisibility(View.INVISIBLE);
+
         return rootView;
     }
 
@@ -36,10 +44,20 @@ public class FriendsFragment extends ListFragment {
     public void onResume() {
         super.onResume();
 
+        List<ParseUser> friends = Global.getFriends();
+        int usersAmount = friends.size();
+        String[] fullNames = new String[usersAmount];
+        int i = 0;
+        for (ParseUser user : friends) {
+            fullNames[i] = user.getString(ParseConstants.KEY_FIRSTNAME) + " " +
+                    user.getString(ParseConstants.KEY_LASTNAME);
+            i++;
+        }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getListView().getContext(),
                 android.R.layout.simple_list_item_1,
-                MainActivity.mFullNames);
+                fullNames);
         setListAdapter(adapter);
     }
 }
